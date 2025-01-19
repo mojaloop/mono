4fa0fc5a8c892ed910dd03740e2da9a090651a06
Kalin Krustev <kalin.krustev@gmail.com>
2025-01-19 21:37:21 +0000

    chore: update test script to create .env file and ensure proper cleanup

--- hub/scripts/test.sh
+++ hub/scripts/test.sh
@@ -5,5 +5,8 @@ set -xe
 docker compose up -d --wait
 docker compose ps
 npm run migrate
+touch .env
 UV_THREADPOOL_SIZE=30 node index.js test provision
 UV_THREADPOOL_SIZE=30 node index.js test gp
+
+docker compose down
