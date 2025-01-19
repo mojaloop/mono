0d3855466be9daa99e3dc4ed7969e4c9533ea4bd
Kalin Krustev <kalin.krustev@gmail.com>
2025-01-19 21:24:12 +0000

    fix: update script to reference correct index.js path

--- hub/scripts/test.sh
+++ hub/scripts/test.sh
@@ -5,5 +5,5 @@ set -xe
 docker compose up -d --wait
 docker compose ps
 npm run migrate
-UV_THREADPOOL_SIZE=30 node hub/index.js test provision
-UV_THREADPOOL_SIZE=30 node hub/index.js test gp
+UV_THREADPOOL_SIZE=30 node index.js test provision
+UV_THREADPOOL_SIZE=30 node index.js test gp
