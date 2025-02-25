7a7b9e13b32ca5829c449b2da5786834ddb632f4
Kalin Krustev <kalin.krustev@gmail.com>
2025-02-25 14:41:28 +0200

    feat: implement WebSocket heartbeat mechanism for connection management

--- mono-log/index.js
+++ mono-log/index.js
@@ -36,6 +36,28 @@ const init = async () => {
 
     const wss = new WebSocketServer({ server: server.listener });
 
+    function heartbeat() {
+        this.isAlive = true;
+    }
+
+    wss.on('connection', function connection(ws) {
+        ws.isAlive = true;
+        ws.on('error', console.error);
+        ws.on('pong', heartbeat);
+    });
+
+    const interval = setInterval(function ping() {
+        wss.clients.forEach(function each(ws) {
+            if (ws.isAlive === false) return ws.terminate();
+            ws.isAlive = false;
+            ws.ping();
+        });
+    }, 30000);
+
+    wss.on('close', function close() {
+        clearInterval(interval);
+    });
+
     const logMessages = [];
 
     const udpServer = dgram.createSocket('udp4');
