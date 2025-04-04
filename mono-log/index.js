95b91d7eda5ebab905e342f4fa1a76b2f6464fa7
Kalin Krustev <kalin.krustev@gmail.com>
2025-04-04 09:07:29 +0300

    feat: add ulidx for unique log message identifiers and enhance filtering functionality

--- mono-log/index.js
+++ mono-log/index.js
@@ -4,6 +4,10 @@ import dgram from 'node:dgram';
 import { WebSocketServer, WebSocket } from 'ws';
 import { LRUCache } from 'lru-cache';
 import split2 from 'split2';
+import { monotonicFactory } from 'ulidx';
+
+const ulid = monotonicFactory();
+const FILTER = Symbol('filter');
 
 const init = async ({
     port = 5170,
@@ -65,21 +69,62 @@ const init = async ({
 
     const logMessages = [];
 
+    const match = filter => log => {
+        if (!filter || !Object.keys(filter).length) return true;
+        for (const key in filter) {
+            switch (key) {
+                case 'ulidStart':
+                    if (log.ulid <= filter[key]) return false;
+                    break;
+                case 'ulidEnd':
+                    if (log.ulid >= filter[key]) return false;
+                    break;
+                default:
+                    if (typeof filter[key] === 'array') {
+                        if (!filter[key].includes(log[key])) return false;
+                    } else
+                        if (log[key] !== filter[key]) return false;
+                    break;
+            }
+        }
+        return true;
+    }
+
+    const filterBy = filter => (!filter || !Object.keys(filter).length) ? logMessages : logMessages.filter(match(filter))
+
+    wss.on('message', function message(msg) {
+        try {
+            const { method, ...rest } = JSON.parse(msg);
+            switch (method) {
+                case 'get':
+                    ws.send(JSON.stringify(filterBy(rest)));
+                    ws[FILTER] = rest;
+                    break;
+                case 'filter':
+                    ws[FILTER] = rest;
+                    break;
+                default:
+                    console.warn(`Unknown method: ${method}`);
+                    break;
+            }
+        } catch (error) {
+            console.error(error, msg);
+        }
+    });
+
     const udpServer = dgram.createSocket('udp4');
 
     const createStream = (id, rinfo) => {
-        const result = split2(/\n/, JSON.parse, {maxLength: 16 * 1024});
+        const result = split2(/\n/, JSON.parse, { maxLength: 16 * 1024 });
         cache.set(id, result);
         result.on('data', msg => {
+            if (!msg.ulid) msg.ulid = ulid();
             logMessages.push(msg);
-            if (logMessages.length > 1000) {
-                logMessages.shift();
-            }
-            console.log(`Received message: ${JSON.stringify(msg)} from ${rinfo.address}:${rinfo.port}`);
+            if (logMessages.length > 1000) logMessages.shift();
 
             wss.clients.forEach(client => {
                 if (client.readyState === WebSocket.OPEN) {
-                    client.send(JSON.stringify(msg));
+                    if (match(client[FILTER])(msg)) client.send(JSON.stringify(msg));
                 }
             });
         });
@@ -98,7 +143,7 @@ const init = async ({
         });
         result.on('close', () => {
             cache.delete(id);
-            udpServer.send(JSON.stringify({method: 'restart'}), rinfo.port, rinfo.address); // notify sender to generate new stream id
+            udpServer.send(JSON.stringify({ method: 'restart' }), rinfo.port, rinfo.address); // notify sender to generate new stream id
         });
         return result;
     };
